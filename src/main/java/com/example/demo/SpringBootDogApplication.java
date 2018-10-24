5adf3b15d7470e60923f120cee1631b350c0a427
carina <cncnewton37@gmail.com>
2018-10-24 16:10:43 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBootDogApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDogApplication.class, args);
+	}
+}
